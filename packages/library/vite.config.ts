8fa66f9259e3cba2d3f7dfbe9e66344e616f1ec3
Sybren W <sybren.willemot@gmail.com>
2024-11-12 22:04:36 +0100

    feat(lib): add vite build config

--- packages/library/vite.config.ts
+++ packages/library/vite.config.ts
@@ -1,3 +1,5 @@
+import { resolve } from 'node:path'
+
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vitest/config'
 
@@ -8,4 +10,23 @@ export default defineConfig({
 		globals: true,
 		environment: 'happy-dom',
 	},
+	build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'TinyForgeUI',
+      fileName: 'lib'
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
 })
